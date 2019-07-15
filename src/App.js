eadd509254ce7d6c9eb314d57584ade6f47eb24e
Daniel Rowe <danielrowemusic@gmail.com>
2019-07-15 13:50:28 -0700

    moved to vexchords draw helper

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className='App'>
       <div className='container'>
-        <div class='cards'>
+        <div className='cards'>
           <ChordDiagram />
           <ChordDiagram />
           <ChordDiagram />

0818a1f8e92792dcda663171a341b453f4dc4364
Daniel Rowe <danielrowetech@gmail.com>
2019-11-09 08:41:30 -0800

    build script

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'PROD') {
+  app.use(express.static('./client/build')); //Serves resources from public folder
+}
+
 const uri = process.env.ATLAS_URI;
 mongoose
   .connect(uri, {
